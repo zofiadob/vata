54a27c10a3bcf6bd87c365ece8a6c5361af5ec6b
zofiadob <z.dobrowolska24@gmail.com>
2023-12-24 22:00:34 +0100

    creating responsive navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,38 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../public/logo.svg'
 import Link from 'next/link'
+import mobileMenuIcon from '../../public/mobileMenu.svg'
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeOpenMobileMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <nav className='flex justify-between px-32 py-2 sticky'>
+    <nav className='flex justify-between px-8 md:px-28 py-2 sticky min-h-16'>
       <div className=''>
-          <Image src={Logo} alt='Logo' />
+        <Image src={Logo} alt='Logo' />
       </div>
 
-      <div className='flex gap-4 justify-center items-center'>
-        <Link href="" className='px-7 font-bold'>About</Link>
-        <Link href="" className='px-7 font-bold'>Values</Link>
-        <Link href="" className='px-7 font-bold'>Pricing</Link>
-        <Link href="" className='px-7 font-bold'>Contact</Link>
+      {isOpen &&
+        <div className='menu-white md:bg-transparent duration-500 md:static absolute md:min-h-fit left-0 top-[100%] md:w-auto w-full flex items-center px-5'>
+          <ul className='flex gap-8 md:gap-2 justify-between items-center lg:w-1/3 md:flex-row flex-col py-4 md:py-0'>
+            <li><Link href="" className='link px-4 font-bold xl:text-lg md:text-md'>About</Link></li>
+            <li><Link href="" className='link px-4 font-bold xl:text-lg md:text-md'>Values</Link></li>
+            <li><Link href="" className='link px-4 font-bold xl:text-lg md:text-md'>Pricing</Link></li>
+            <li><Link href="" className='link px-4 font-bold xl:text-lg md:text-md'>Contact</Link></li>
+          </ul>
         </div>
+      }
+
+      <div className='flex items-center md:hidden'>
+        <Image onClick={closeOpenMobileMenu} className='cursor-pointer' src={mobileMenuIcon} alt={'Menu icon'} width='40' height="40" />
+      </div>
     </nav>
   )
 }
