53f8a721ed35e73e85cef489c1a09a158d9dfb4b
zofiadob <z.dobrowolska24@gmail.com>
2024-01-01 21:11:19 +0100

    adding option for custom height line

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,24 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      lineHeight: {
+        'extra-loose': '3.5rem',
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+      },
       colors: {
         transparent: 'transparent',
         'white': '#ffffff',
