a0d3def56c36a8f27772a6b06ff1e8303dda8dec
zofiadob <z.dobrowolska24@gmail.com>
2024-01-03 22:07:34 +0100

    adding style + responsivness

--- app/components/Sections/ValueSection.tsx
+++ app/components/Sections/ValueSection.tsx
@@ -28,18 +28,17 @@ function ValueSection() {
 
   return (
     <section id='values' className='px-8 md:px-28 py-10'>
-      <div className='grid sm:grid-cols-1 items-center gap-5 md:gap-12 lg:gap-20'>
-        <div className=''>
-          <div className='grid grid-cols-1'>
-            <Header text={HeaderText} align='left' />
-            <Text text={desciptionText} />
-          </div>
+      <div className='grid sm:grid-cols-1 items-center gap-5 md:gap-12'>
+        <div className='grid grid-cols-1'>
+          <Header text={HeaderText} align='left' />
+          <Text text={desciptionText} />
         </div>
-        <div className='grid grid-cols-2 md:grid-cols-4'>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8'>
           {
             values.map(value => (
-              <div key={value.name}>
-                {value.name}
+              <div key={value.name} className='bg-white text-main-violet text-center rounded-2xl p-4'>
+                <div className='text-md font-bold md:text-xl lg:text-2xl mb-1 md:mb-4'>{value.name}</div>
+                <div className='text-sm md:text-md'>{value.desc}</div>
               </div>
             ))
           }
