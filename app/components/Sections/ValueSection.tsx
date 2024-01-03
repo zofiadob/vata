840f0e190be46cb76b61003aefdba4bf82ca00af
zofiadob <z.dobrowolska24@gmail.com>
2024-01-03 20:50:27 +0100

    adding content to value section

--- app/components/Sections/ValueSection.tsx
+++ app/components/Sections/ValueSection.tsx
@@ -1,10 +1,49 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
+import Header from '../Header';
+import Text from '../Text';
 
 function ValueSection() {
+
+  const HeaderText = 'Our values'
+  const desciptionText = 'At Vata, we are committed to delivering unparalleled value to our clients through:'
+
+  const values = [
+    {
+      name: "Precision",
+      desc: "Harnessing the power of advanced analytics to provide you with accurate and reliable insights.",
+    },
+    {
+      name: "Innovation",
+      desc: "Staying at the forefront of technological advancements to keep your business ahead of the curve.",
+    },
+    {
+      name: "Collaboration",
+      desc: "Working closely with you to understand your unique challenges and tailor solutions that align with your goals.",
+    },
+    {
+      name: "Integrity",
+      desc: "Upholding the highest ethical standards to ensure the confidentiality and security of your data.",
+    },
+  ]
+
   return (
-    <section>
-      <div>
-        ValueSection
+    <section id='values' className='px-8 md:px-28 py-10'>
+      <div className='grid sm:grid-cols-1 items-center gap-5 md:gap-12 lg:gap-20'>
+        <div className=''>
+          <div className='grid grid-cols-1'>
+            <Header text={HeaderText} align='left' />
+            <Text text={desciptionText} />
+          </div>
+        </div>
+        <div className='grid grid-cols-2 md:grid-cols-4'>
+          {
+            values.map(value => (
+              <div key={value.name}>
+                {value.name}
+              </div>
+            ))
+          }
+        </div>
       </div>
     </section>
   )
